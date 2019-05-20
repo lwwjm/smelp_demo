42f1f979f0003f7cb096554fa4a6d1c74fa455db
lw <lw@example.com>
2019-05-20 09:46:12 +0800

    spring

---
+++ eclipse-workspace/smelp_spring/src/main/java/com/smelp/spring/SmelpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.smelp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmelpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmelpSpringApplication.class, args);
+	}
+
+}
